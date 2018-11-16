aea9d9db875f09612d9587e3b8b4a01e67919ee1
Tyler Boright <lupin3.ken@gmail.com>
2018-11-16 15:18:47 -0800

    adds test to check if line breaks exist anymore

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestConvertString(t *testing.T) {
+	// Makes sure there are no explicit line blocks in the string
+	testStr := `I
+  Am
+  A
+  Test`
+
+	convertedStr := ConvertString(testStr)
+
+	for i, r := range convertedStr {
+		if r == 10 {
+			t.Errorf("We did not eliminate the line breaks correctly!!  Line break at index: %d", i)
+		}
+	}
+}
