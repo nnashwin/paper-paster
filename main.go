8fbdc50defd88c7ac8fe34c190b5a7f9fe5b5dbb
Tyler Boright <lupin3.ken@gmail.com>
2018-11-16 14:59:02 -0800

    adds implementation to add extra spaces after the removal of the byte 10 (newline) happens

--- main.go
+++ main.go
@@ -2,39 +2,64 @@ package main
 
 import "fmt"
 
+func convertString(str string) string {
+
+	return ""
+}
+
 func main() {
-	str := `The basic concept of fractals
- is that they contain
- a large degree of
-self similarity
-. This means
- that they
- usually
- contain little copi
-es of
-themselves
-  buried   deep   within   the   original
-  like   the   stars
-embedded
- in the Koch Snowflake
-[17]
- shown in 
- Figure 1
-. Also,
-fractals
- possess infinite
- detail, so for any given fractal the closer
-we look at it the more detail it 
-can reveal. 
-[20]`
+	str := `Like any procedural technique
+, a fractal shape is defined
+ by an
+algorithm
+ for generating
+ the shape. In the case of fractals
+ these
+algorithms
+ are recursive
+ and successive
+ recursions
+ yield more
+detailed
+ versions
+ of the basic shape. The example of the Koch
+snowflake in Figure 2 shows four such recursions.
+ Self-similarit
+y
+is achieved
+ by generating
+ the same shapes or patterns
+ at smaller
+and smaller
+ scales as the recursion
+ progresses,
+ a propert
+y often
+referred
+ to as scale invariance.
+ There is no theoretical
+ limit to the
+amount
+ of recursion
+ that can be done and hence infinite
+ levels of
+detail exist within the shape.`
 
 	bs := []byte(str)
 
 	for i, r := range bs {
 		if r == 10 {
+			s := append(bs[:i], byte(32))
+			bs = append(s, bs[i+1:]...)
+		}
+	}
+
+	for i := len(bs) - 1; i >= 0; i-- {
+		if bs[i] == 32 && bs[i+1] == 32 {
 			bs = append(bs[:i], bs[i+1:]...)
 		}
 	}
+
 	fmt.Println(bs)
 
 	s := string(bs)
